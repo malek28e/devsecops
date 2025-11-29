6025e1cf43bcec7e30ba1bb1efeb33635723b09f
malek28e <malek.arbi@esprit.tn>
2025-11-29 11:02:57 +0100

    init

--- src/main/java/tn/esprit/spring/entities/Entreprise.java
+++ src/main/java/tn/esprit/spring/entities/Entreprise.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-//khiylgbjpoyhjj
+//khiylgbjpoysdsdhjjggd
 @Entity
 public class Entreprise implements Serializable{
 	
